f920dc17be436b0559f3c05dd0fb50b4f7f49d72
jithinraj9895 <jithinraj9895@gmail.com>
2021-09-28 15:17:22 +0530

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const app = express()
+const port = 3000
 
 app.get("/",function(req, res){
     res.send("HELLO, LADIES AND GENTLEMEN")
 })
 
-app.listen(3000)
+app.listen(process.env.PORT || port)
